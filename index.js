c0665b8eefdc2515c680abc06737a8c6745c8af2
Sabrina <Smg21022@gmail.com>
2023-09-25 19:50:24 -0700

    finally works

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-require("dotenv").config();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
 
-server = require("./server");
+const server = express()
 
-const port = process.env.PORT;
+const PORT = process.env.PORT || 9000
 
-server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/hello', (req, res, next) => {
+    res.json({ message: 'api is working '})
+})
+
+server.use('*', (req, res) => {
+    res.send(`<h1>Hello, there!</h1>`)
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
 })
